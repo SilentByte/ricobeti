2c1bbbc4f854c29609a331ecc9937ac0b23ed96d
Rico Beti <rico.beti@silentbyte.com>
2017-07-08 03:07:15 +0800

    Fix anchor scroll offset caused by fixed navbar

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -11,7 +11,7 @@
         $('a.anchor-scroll').bind('click', function(event) {
             var anchor = $(this);
             $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
+                scrollTop: $(anchor.attr('href')).offset().top - 64
             }, 1500, 'easeInOutExpo');
             event.preventDefault();
         });
